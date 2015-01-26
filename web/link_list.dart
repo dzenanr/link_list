fb8ec1ff7b77b5af5cb031cfd94c2ab6acd626f4
Dzenan Ridjanovic <dzenanr@gmail.com>
2015-01-26 11:10:51 -0500

    add web directory

--- web/link_list.dart
+++ web/link_list.dart
@@ -1,8 +1,23 @@
-library link_list;
-
 import 'dart:html';
 
-part 'links.dart';
+List links = new List();
+
+class Link {
+  var name, url, description;
+}
+
+init() {
+  var link1 = new Link();
+  link1.name = "dartling";
+  link1.url = "https://github.com/dzenanr/dartling";
+  link1.description = "Domain model framework";
+  links.add(link1);
+  var link2 = new Link();
+  link2.name = "Dart";
+  link2.url = "http://www.dartlang.org/";
+  link2.description = "Web programming language";
+  links.add(link2);
+}
 
 display() {
   String list = '';
